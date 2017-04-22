f77f83d2049b4b45098897946722c3f2ca9bb706
Greg Lee <gml2136@gmail.com>
2017-04-21 22:49:50 -0700

    more heroku stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //serverFTW
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORt || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
